a667c25af91cd15c84099b3fd16092dbd70ddb9e
Mochammad Fauzi Alvarizi <127657573+Fauzi002@users.noreply.github.com>
2024-03-20 09:43:00 +0700

    list display data

--- mysite/dashboard/admin.py
+++ mysite/dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from dashboard.models import pegawai
 # Register your models here.
+
+@admin.register(pegawai)
+class pegawaiAdmin(admin.ModelAdmin):
+    list_display = ['nama', 'alamat', 'jenis_kelamin', 'golongan']
